dc226dc56178610395b59c053ddfd1d0be54f706
Farai Wande <faraiwande@gmail.com>
2023-10-31 15:53:11 +0000

    addition of items using POST

--- todo_app/app.py
+++ todo_app/app.py
@@ -1,14 +1,19 @@
-from flask import Flask
-
-from flask import render_template
+from flask import Flask, render_template, request
 from todo_app.flask_config import Config
-from todo_app.data.session_items import get_items
+from todo_app.data.session_items import get_items, add_item
 
 app = Flask(__name__)
 app.config.from_object(Config())
 
 
-@app.route('/')
+@app.route('/',methods=['GET','POST'])
 def index():
-    items = get_items()
+    if request.method =='POST':
+        add_item(request.form.get('title'))
+        items = get_items()
+    else:
+        items = get_items()
+
     return render_template ('index.html', my_items = items)
+
+
