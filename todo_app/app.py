483263fea9e81996a7077b79b23b3353e21dbcb8
Farai Wande <faraiwande@gmail.com>
2023-10-31 17:40:37 +0000

    completed the add task feature

--- todo_app/app.py
+++ todo_app/app.py
@@ -1,4 +1,4 @@
-from flask import Flask, render_template, request
+from flask import Flask, request,render_template,  url_for 
 from todo_app.flask_config import Config
 from todo_app.data.session_items import get_items, add_item
 
@@ -6,14 +6,20 @@ app = Flask(__name__)
 app.config.from_object(Config())
 
 
-@app.route('/',methods=['GET','POST'])
-def index():
-    if request.method =='POST':
-        add_item(request.form.get('title'))
-        items = get_items()
-    else:
-        items = get_items()
+     
 
-    return render_template ('index.html', my_items = items)
+@app.route('/', methods=['GET','POST'] )
+def index():
+        items = get_items()
+        return render_template ('index.html', my_items = items)
+        
+
+@app.route('/add_tasks', methods=['POST'])
+def add_tasks():
+        add_item(dict(request.form.items())['title'])
+        items = get_items()
+        return render_template ('index.html', my_items = items)
+        
+    
 
 
