21706ebfe7dc7f3a63e98f4e1a872fa6a6f7b599
Farai Wande <faraiwande@outlook.com>
2024-11-12 11:44:14 +0000

    addition of variable.tf & also addition of DB including the collection to the Cosmos DB

--- terraform/main.tf
+++ terraform/main.tf
@@ -1,7 +1,7 @@
 terraform {
   required_providers {
     azurerm = {
-      source = "hashicorp/azurerm"
+      source  = "hashicorp/azurerm"
       version = ">= 3.8"
     }
   }
@@ -9,11 +9,11 @@ terraform {
 
 provider "azurerm" {
   features {}
-  subscription_id = "d33b95c7-af3c-4247-9661-aa96d47fccc0"
+  subscription_id = var.SUBSCRIPTION_ID
 }
 
 data "azurerm_resource_group" "main" {
-  name     = "Cohort30_FarWan_ProjectExercise"
+  name = var.RESOURCE_GROUP_NAME
 }
 
 
@@ -33,8 +33,82 @@ resource "azurerm_linux_web_app" "main" {
 
   site_config {
     application_stack {
-      docker_image_name     = "appsvcsample/python-helloworld:latest"
-      docker_registry_url   = "https://index.docker.io"
+      docker_image_name   = "appsvcsample/python-helloworld:latest"
+      docker_registry_url = "https://index.docker.io"
+
     }
+  }
+
+  app_settings = {
+    FLASK_APP                           = var.FLASK_APP
+    FLASK_DEBUG                         = var.FLASK_DEBUG
+    SECRET_KEY                          = var.SECRET_KEY
+    WEBSITES_ENABLE_APP_SERVICE_STORAGE = var.WEBSITES_ENABLE_APP_SERVICE_STORAGE
+    WEBSITES_PORT                       = var.WEBSITES_PORT
+    MONGODB_COLLECTION_NAME             = var.MONGODB_COLLECTION_NAME
+    MONGODB_NAME                        = var.MONGODB_NAME
+    MONGODB_CONNECTIONSTRING            = azurerm_cosmosdb_account.main.primary_mongodb_connection_string
+
+  }
+}
+
+resource "azurerm_cosmosdb_account" "main" {
+  name                = var.COSMOSDB_ACCOUNT
+  location            = data.azurerm_resource_group.main.location
+  resource_group_name = data.azurerm_resource_group.main.name
+  offer_type          = "Standard"
+  kind                = "MongoDB"
+
+
+  capabilities {
+    name = "MongoDBv3.4"
+  }
+
+  capabilities {
+    name = "EnableMongo"
+  }
+  capabilities {
+    name = "EnableServerless"
+  }
+
+  lifecycle { prevent_destroy = true }
+
+  consistency_policy {
+    consistency_level       = "BoundedStaleness"
+    max_interval_in_seconds = 300
+    max_staleness_prefix    = 100000
+  }
+
+  geo_location {
+    location          = "uksouth"
+    failover_priority = 0
+  }
+
+}
+
+data "azurerm_cosmosdb_account" "db" {
+  name                = var.COSMOSDB_ACCOUNT
+  resource_group_name = var.RESOURCE_GROUP_NAME
+}
+
+resource "azurerm_cosmosdb_mongo_database" "db" {
+  name                = var.MONGODB_NAME
+  resource_group_name = data.azurerm_cosmosdb_account.db.resource_group_name
+  account_name        = data.azurerm_cosmosdb_account.db.name
+
+}
+
+resource "azurerm_cosmosdb_mongo_collection" "db" {
+  name                = var.MONGODB_COLLECTION_NAME
+  resource_group_name = data.azurerm_cosmosdb_account.db.resource_group_name
+  account_name        = data.azurerm_cosmosdb_account.db.name
+  database_name       = azurerm_cosmosdb_mongo_database.db.name
+
+  default_ttl_seconds = "777"
+  shard_key           = "uniqueKey"
+
+  index {
+    keys   = ["_id"]
+    unique = true
   }
 }
---
+++ terraform/variable.tf
@@ -0,0 +1,46 @@
+variable "FLASK_APP" {
+  type = string
+}
+
+variable "FLASK_DEBUG" {
+  type = string
+}
+
+variable "WEBSITES_ENABLE_APP_SERVICE_STORAGE" {
+  type = string
+}
+
+variable "SECRET_KEY" {
+  type = string
+}
+
+variable "WEBSITES_PORT" {
+  type = string
+}
+
+variable "MONGODB_COLLECTION_NAME" {
+  type = string
+
+}
+
+variable "MONGODB_NAME" {
+  type = string
+
+}
+
+variable "SUBSCRIPTION_ID" {
+  type = string
+  sensitive = true
+
+}
+
+variable "RESOURCE_GROUP_NAME" {
+  type = string
+  sensitive = true
+
+}
+
+variable "COSMOSDB_ACCOUNT" {
+    type = string
+  
+}
