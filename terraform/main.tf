76d80243e5a9d12853d10a0f4924c3eed3308e2e
Farai Wande <faraiwande@outlook.com>
2024-11-12 10:26:47 +0000

    service plan & app service

--- terraform/main.tf
+++ terraform/main.tf
@@ -12,6 +12,29 @@ provider "azurerm" {
   subscription_id = "d33b95c7-af3c-4247-9661-aa96d47fccc0"
 }
 
-data "azurerm_resource_group" "rg" {
+data "azurerm_resource_group" "main" {
   name     = "Cohort30_FarWan_ProjectExercise"
+}
+
+
+resource "azurerm_service_plan" "main" {
+  name                = "todo_app_appservice_plantf"
+  location            = data.azurerm_resource_group.main.location
+  resource_group_name = data.azurerm_resource_group.main.name
+  os_type             = "Linux"
+  sku_name            = "B1"
+}
+
+resource "azurerm_linux_web_app" "main" {
+  name                = "fwtodoapptf"
+  location            = data.azurerm_resource_group.main.location
+  resource_group_name = data.azurerm_resource_group.main.name
+  service_plan_id     = azurerm_service_plan.main.id
+
+  site_config {
+    application_stack {
+      docker_image_name     = "appsvcsample/python-helloworld:latest"
+      docker_registry_url   = "https://index.docker.io"
+    }
+  }
 }
