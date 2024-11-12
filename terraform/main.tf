f458a7b2d5fcf23c51d5e0410bf64a28fc2fea4b
Farai Wande <faraiwande@outlook.com>
2024-11-12 16:16:50 +0000

    db resource

--- terraform/main.tf
+++ terraform/main.tf
@@ -109,7 +109,7 @@ resource "azurerm_cosmosdb_mongo_collection" "db" {
   name                = var.MONGODB_COLLECTION_NAME
   resource_group_name = data.azurerm_cosmosdb_account.db.resource_group_name
   account_name        = data.azurerm_cosmosdb_account.db.name
-  database_name       = data.azurerm_cosmosdb_mongo_database.db.name
+  database_name       = azurerm_cosmosdb_mongo_database.db.name
 
 
   index {
